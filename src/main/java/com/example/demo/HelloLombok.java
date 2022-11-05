bf72d5e8d04a17fe63fca90436a7d5a39f0484b9
msung99 <msung6924@naver.com>
2022-11-05 12:52:56 +0900

    feat: lombok

---
+++ src/main/java/com/example/demo/HelloLombok.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String name;
+    private int age;
+
+
+    /*
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    } */
+
+    public static void main(String[] args){
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asfsa");
+
+        String name = helloLombok.getName();
+        System.out.println("name = " + name);
+    }
+}
--- src/main/java/com/example/demo/Order/OrderServiceImpl.java
+++ src/main/java/com/example/demo/Order/OrderServiceImpl.java
@@ -12,13 +12,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository; // = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; // = new MemoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy; // = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy; // = new FixDiscountPolicy();
     // DIP 의존관계를 만족 => 구현체에도 의존하던 클라이언트가, 이제는
     // 인터페이스에만 의존하도록 변경되었다.
 
+    /*
     @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -28,6 +29,7 @@ public class OrderServiceImpl implements OrderService{
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
+    */
 
     // OrderServiceImpl 입장에서는 생성자를 통해 어떤 구현 객체가 들어올지(주입될지)는 알 수 없다.
     // 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부(AppConfig) 에서 결정한다.
