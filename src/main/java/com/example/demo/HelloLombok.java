fea45123190a837c95950265f0cad6d4632ac85e
msung99 <msung6924@naver.com>
2022-11-05 13:00:03 +0900

    Refactor: lombok RequiredArgsConstructor

--- src/main/java/com/example/demo/HelloLombok.java
+++ src/main/java/com/example/demo/HelloLombok.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class HelloLombok {
     private String name;
     private int age;
@@ -31,7 +33,8 @@ public class HelloLombok {
         HelloLombok helloLombok = new HelloLombok();
         helloLombok.setName("asfsa");
 
-        String name = helloLombok.getName();
-        System.out.println("name = " + name);
+        // String name = helloLombok.getName();
+        //System.out.println("name = " + name);
+        System.out.println("name = " + helloLombok);
     }
 }
--- src/main/java/com/example/demo/Order/OrderServiceImpl.java
+++ src/main/java/com/example/demo/Order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.member.Member;
 import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // lombok 의 기능 => 생성자를 자동생성
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; // = new MemoryMemberRepository();
@@ -18,7 +20,6 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy; // = new FixDiscountPolicy();
     // DIP 의존관계를 만족 => 구현체에도 의존하던 클라이언트가, 이제는
     // 인터페이스에만 의존하도록 변경되었다.
-
     /*
     @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
@@ -33,11 +34,13 @@ public class OrderServiceImpl implements OrderService{
 
     // OrderServiceImpl 입장에서는 생성자를 통해 어떤 구현 객체가 들어올지(주입될지)는 알 수 없다.
     // 생성자를 통해서 어떤 구현 객체를 주입할지는 오직 외부(AppConfig) 에서 결정한다.
-    @Autowired
+
+    /*
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
 
     public MemberRepository getMemberRepository() {
         return memberRepository;
